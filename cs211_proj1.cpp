5696d12c16b2ddcae1a679bbc05e8cbebe5f9fcf
ngowd001 <ngowd001@ucr.edu>
2018-10-12 14:47:37 -0700

    update correct output lines

--- cs211_proj1.cpp
+++ cs211_proj1.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include "time.h""
+#include "time.h"
 #include "cstdlib"
+
 using namespace std;
 
 void RandomizeArrays(double arr1[], double arr2[], int array_size)
 {
 	int m = 0;
+
 	for (m = 0; m < array_size; ++m)
 	{
 		arr1[m] = rand() % 100;
 		arr2[m] = rand() % 100;
-
 	}
 }
+
 
 
 
@@ -22,20 +24,30 @@ int main(int argc, char** argv)
 	int j = 0;
 	int k = 0;
 
+
 	const int n = atoi(argv[1]);
 
 	const int arr_size = n * n;
 
+
 	double C[arr_size];
 	double B[arr_size];
 	double A[arr_size];
-	
+
+
+
 	RandomizeArrays(A, B, arr_size);
 
+
+
 	clock_t t;
+
 	t = clock();
 
+
+
 	/* dgemm(): simple ijk version triple loop algorithm */
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
@@ -49,16 +61,14 @@ int main(int argc, char** argv)
 
 	t = clock() - t;
 
-
-	cout << "For dgemm0 and n size of " << arr_size << " we have clock time of " << t << endl;
-
-	
+	cout << "For dgemm0 and n size of " << n << " we have clock time of " << t << endl;
 
 	RandomizeArrays(A, B, arr_size);
 
 	t = clock();
 
 	/* dgemm1: simple ijk version triple loop algorithm with register reuse */
+
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
@@ -74,6 +84,6 @@ int main(int argc, char** argv)
 
 	t = clock() - t;
 
-	cout << "For dgemm1 and n size of " << arr_size << " we have clock time of " << t << endl << endl;
+	cout << "For dgemm1 and n size of " << n << " we have clock time of " << t << endl << endl;
 
 }
