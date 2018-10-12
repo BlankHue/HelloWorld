0ad5d748f99664afde9f500006b09dd9b2e22e21
ngowd001 <ngowd001@ucr.edu>
2018-10-12 01:51:34 -0700

    pushing changes

--- cs211_proj1.sh
+++ cs211_proj1.sh
@@ -6,4 +6,15 @@
 #SBATCH -t 00:10:00
 #SBATCH -w, --nodelist=node01
 
-./cs211_proj1
+./cs211_proj1 64
+
+./cs211_proj1 128
+ 
+./cs211_proj1 256
+
+./cs211_proj1 512
+
+./cs211_proj1 1024
+
+./cs211_proj1 2048
+
