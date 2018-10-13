43a2570cd78c337a761ad353c2c0a997fc033157
ngowd001 <ngowd001@ucr.edu>
2018-10-12 18:40:36 -0700

    added new bash file

--- cs211_proj1.sh
+++ cs211_proj1.sh
@@ -3,17 +3,17 @@
 #SBATCH --output=res.txt
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 00:10:00
+#SBATCH -t 00:20:00
 
-./cs211_proj1 64
+./cs211_proj1_1_1 64
 
-./cs211_proj1 128
+./cs211_proj1_1_1 128
  
-./cs211_proj1 256
+./cs211_proj1_1_1 256
 
-./cs211_proj1 512
+./cs211_proj1_1_1 512
 
-./cs211_proj1 1024
+./cs211_proj1_1_1 1024
 
-./cs211_proj1 2048
+./cs211_proj1_1_1 2048
 
