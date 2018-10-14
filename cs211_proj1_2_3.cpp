77691cd09ee9a81431fca5ac7c27de20a3e13d2f
ngowd001 <ngowd001@ucr.edu>
2018-10-14 12:54:36 -0700

    Update cs211_proj1_2_3.cpp

--- cs211_proj1_2_3.cpp
+++ cs211_proj1_2_3.cpp
@@ -1,6 +1,6 @@
 #include <ctime>
-#include <cstdlib>
 #include <iostream>
+#include <cstdlib>
 
 
 
@@ -35,33 +35,24 @@ int main(int argc, char** argv)
 	double * B;
 	double * A;
 
-
 	const int n = atoi(argv[1]);
 	const int arr_size = n * n;
-
-
 
 	C = (double*)malloc(arr_size * sizeof(double));
 	B = (double*)malloc(arr_size * sizeof(double));
 	A = (double*)malloc(arr_size * sizeof(double));
 
-
 	RandomizeArrays(A, B, arr_size);
 
-
 	clock_t t;
-
-
 
 	cout << "For ijk and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
 
 	t = clock();
 
 	/* ijk */
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++) 
-		{
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
 			register double sum = 0.0;
 			for (k = 0; k < n; k++)
 			{
@@ -71,17 +62,9 @@ int main(int argc, char** argv)
 		}
 	}
 
-	
 	t = clock() - t;
 
-
-
-	cout << "For dgemm3 and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
-
-
-
-	/* Multiply n x n matrices a and b  */
-
+	cout << "For ijk and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
 
 
 }
