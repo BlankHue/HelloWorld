d36b9aa25444889a9375bd93a3665307088e097d
ngowd001 <ngowd001@ucr.edu>
2018-10-14 12:04:16 -0700

    Update cs211_proj1.sh

--- cs211_proj1.sh
+++ cs211_proj1.sh
@@ -5,8 +5,23 @@
 #SBATCH -n 1
 #SBATCH -t 00:20:00
 
+foo=bar
+:'
+./cs211_proj1_2_3 64
 
+./cs211_proj1_2_3 128
 
+./cs211_proj1_2_3 256
+
+./cs211_proj1_2_3 512
+
+./cs211_proj1_2_3 1024
+'
+
+./cs211_proj1_2_3 2048
+
+foo=bar
+:'
 ./cs211_proj1_1_3 64
 
 ./cs211_proj1_1_3 128
@@ -20,8 +35,6 @@
 ./cs211_proj1_1_3 2048
 
 
-foo=bar
-: '
 ./cs211_proj1_1_1 64
 
 ./cs211_proj1_1_1 128
