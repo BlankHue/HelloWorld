0aca1659c890b693b77614933f3fb0836a58e89e
ngowd001 <ngowd001@ucr.edu>
2018-10-16 13:08:24 -0700

    Update cs211_proj1_2_3.cpp

--- cs211_proj1_2_3.cpp
+++ cs211_proj1_2_3.cpp
@@ -67,21 +67,36 @@ int main(int argc, char** argv)
 
 	t = clock();
 
+	int block = 8; // block size
+
 	/* ijk */
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < n; j++) {
-			register double sum = 0.0;
-			for (k = 0; k < n; k++)
+
+	for (i = 0; i < n; i += block)
+	{
+		for (j = 0; j < n; j += block)
+		{
+			for (k = 0; k < n; k += block)
 			{
-				sum += A[i*n+k] * B[k*n+j];
+				for (int i1 = i; i1 < i + block; i1++)
+				{
+					for (int j1 = j; j1 < j + block; j1++)
+					{
+						register double r = C[i1*n + j1];
+						for (int k1 = k; k1 < k + block; k1++)
+						{
+							r += A[i1*n + k1] * B[k1*n + j1];
+						}
+						C[i1*n + j1] = r;
+					}
+				}
 			}
-			C[i*n+j] = sum;
 		}
 	}
 
 	t = clock() - t;
 
-	cout << "For ijk and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
+	cout << "For ijk cache block and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
+	
 
 	storeMax(C, check_array, 0, arr_size); //stores max of solution matrix in an array at index 0
 
@@ -89,23 +104,34 @@ int main(int argc, char** argv)
 
 	t = clock();
 
-	/* jik */
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n; j += block)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i < n; i += block)
 		{
-			register double sum = 0.0;
-			for (k = 0; k < n; k++)
+			for (k = 0; k < n; k += block)
 			{
-				sum += A[i*n + k] * B[k*n + j];
+				for (int j1 = j; j1 < j + block; j1++)
+				{
+					for (int i1 = i; i1 < i + block; i1++)
+					{
+						register double r = C[j1*n + i1];
+						for (int k1 = k; k1 < k + block; k1++)
+						{
+							r += A[j1*n + i1] * B[i1*n + k1];
+						}
+						C[j1*n + i1] = r;
+					}
+				}
 			}
-			C[i*n + j] = sum;
 		}
 	}
+
 
 	t = clock() - t;
 
 	cout << "For jik and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
+
+
 
 	storeMax(C, check_array, 1, arr_size); //stores max of solution matrix in an array at index 0
 
@@ -113,16 +139,17 @@ int main(int argc, char** argv)
 	{
 		cout << "Correctness: TRUE" << endl;
 	}
-	else 
+	else
 	{
 		cout << "Correctness: FALSE " << endl << "first value: " << check_array[0] << endl << "second value: " << check_array[1] << endl;
 	}
 
+	/*
 	RandomizeArrays(A, B, C, arr_size);
 
 	t = clock();
 
-	/* kij */
+	// kij
 	for (k = 0; k < n; k++)
 	{
 		for (i = 0; i < n; i++)
@@ -139,6 +166,7 @@ int main(int argc, char** argv)
 
 	cout << "For kij and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
 
+	
 	storeMax(C, check_array, 2, arr_size);
 
 	if (check_array[1] == check_array[2])
@@ -147,15 +175,15 @@ int main(int argc, char** argv)
 	}
 	else
 	{
-		cout << "Correctness: FALSE " << endl << "first value: " << check_array[1] << endl << "second value: " << check_array[2] <<  endl;
+		cout << "Correctness: FALSE " << endl << "first value: " << check_array[1] << endl << "second value: " << check_array[2] << endl;
 	}
 
 
 	RandomizeArrays(A, B, C, arr_size);
-	
+
 	t = clock();
 
-	/* ikj */
+	// ikj
 	for (i = 0; i < n; i++)
 	{
 		for (k = 0; k < n; k++)
@@ -169,7 +197,7 @@ int main(int argc, char** argv)
 	}
 
 	t = clock() - t;
-	
+
 	cout << "For ikj and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
 
 	storeMax(C, check_array, 3, arr_size);
@@ -188,7 +216,7 @@ int main(int argc, char** argv)
 
 	t = clock();
 
-	/* jki */
+	// jki
 	for (j = 0; j < n; j++)
 	{
 		for (k = 0; k < n; k++)
@@ -220,7 +248,7 @@ int main(int argc, char** argv)
 
 	t = clock();
 
-	/* kji */
+	// kji
 	for (k = 0; k < n; k++)
 	{
 		for (j = 0; j < n; j++)
@@ -236,7 +264,7 @@ int main(int argc, char** argv)
 	t = clock() - t;
 
 	cout << "For kji and n size of " << n << " we have clock time of " << (double(t) / CLOCKS_PER_SEC) << endl;
-
+	ca
 	storeMax(C, check_array, 5, arr_size);
 
 	if (check_array[3] == check_array[4])
@@ -247,5 +275,5 @@ int main(int argc, char** argv)
 	{
 		cout << "Correctness: FALSE " << endl << "first value: " << check_array[1] << endl << "second value: " << check_array[2] << endl;
 	}
-
+	*/
 }
